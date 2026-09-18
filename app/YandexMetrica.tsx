db086496aa99f1508506ff37e21eac1d62fcf112
Fentoph <fentoph@proton.me>
2026-09-19 04:31:48 +0500

    fix: correct Yandex Metrica script URL JSX syntax

--- app/YandexMetrica.tsx
+++ app/YandexMetrica.tsx
@@ -1,3 +1,55 @@
 "use client";
+
 import Script from "next/script";
-export default function YandexMetrica(){const id=process.env.NEXT_PUBLIC_YANDEX_METRICA_ID;if(!id)return null;return <><Script id="yandex-metrica-loader" strategy="afterInteractive" src="https://mc.yandex.ru/metrika/tag.js?id="+id onLoad={()=>{const w=window as unknown as {ym?:Function};w.ym=w.ym||function(...args:unknown[]){(w.ym as any).a=((w.ym as any).a||[]).concat([args])};w.ym(Number(id),"init",{ssr:true,webvisor:true,clickmap:true,accurateTrackBounce:true,trackLinks:true})}}/><noscript><div><img src={"https://mc.yandex.ru/watch/"+id} style={{position:"absolute",left:"-9999px"}} alt=""/></div></noscript></>}
+
+export default function YandexMetrica() {
+  const id = process.env.NEXT_PUBLIC_YANDEX_METRICA_ID;
+
+  if (!id) return null;
+
+  const scriptSrc = `https://mc.yandex.ru/metrika/tag.js?id=${encodeURIComponent(id)}`;
+  const imageSrc = `https://mc.yandex.ru/watch/${encodeURIComponent(id)}`;
+
+  return (
+    <>
+      <Script
+        id="yandex-metrica-loader"
+        strategy="afterInteractive"
+        src={scriptSrc}
+        onLoad={() => {
+          const w = window as unknown as {
+            ym?: ((...args: unknown[]) => void) & { a?: unknown[] };
+          };
+
+          w.ym =
+            w.ym ||
+            Object.assign(
+              (...args: unknown[]) => {
+                w.ym!.a = w.ym!.a || [];
+                w.ym!.a.push(args);
+              },
+              { a: [] },
+            );
+
+          w.ym(Number(id), "init", {
+            ssr: true,
+            webvisor: true,
+            clickmap: true,
+            accurateTrackBounce: true,
+            trackLinks: true,
+            ecommerce: "dataLayer",
+          });
+        }}
+      />
+      <noscript>
+        <div>
+          <img
+            src={imageSrc}
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+    </>
+  );
+}
